983a8bec1fa820dfbc146a8c1f2494e4f01c8a68
bryan aviles <easyrun32@gmail.com>
2021-03-14 18:21:01 -0400

    position setup

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import GenerateUser from "./component/generate-user/generate-user.component";
 import GenerateAllUsers from "./component/generate-all-users/generate-all-users.component";
+import MoveUser from "./component/move-user/move-user.component";
 /*
 This version of parcel is deperacated but parcel is lightweight
 
@@ -13,6 +13,9 @@ const App = () => {
     <div>
       <div className="center">
         <GenerateAllUsers storeId={"psq1"} manager={"felipe"} />
+       
+        <MoveUser id={4545} storeId={"psq1"} role={"driver"} />
+        <MoveUser id={ 8954} storeId={"psq1"} role={"driver"} />
       </div>
     </div>
   );
--- component/move-user/move-user.component.jsx
+++ component/move-user/move-user.component.jsx
@@ -5,23 +5,27 @@ import Button from "@material-ui/core/Button";
 Test component that takes @params  id 
 Description: Simulates a driver moving
 */
-const MoveUser = ({ id }) => {
+const MoveUser = ({ id, storeId, role }) => {
   const [socket, setSocket] = useState();
   const [connected, setConnected] = useState(true);
   const [userexist, setUserexist] = useState(true);
   const [position, setPosition] = useState({
-    coords: {
-      accuracy: 65,
-      altitude: 210.50143432617188,
-      altitudeAccuracy: 10,
-      heading: -1,
-      latitude: 26.259,
-      longitude: -80.27,
-      speed: -1,
+    position: {
+      coords: {
+        accuracy: 65,
+        altitude: 210.50143432617188,
+        altitudeAccuracy: 10,
+        heading: -1,
+        latitude: 26.259,
+        longitude: -80.27,
+        speed: -1,
+      },
+      timestamp: 1610579477507.111,
     },
-    timestamp: 1610579477507.111,
+    storeId: storeId,
+    id: id,
   });
-  // to get prevstate, see line 58 to explain why we get prevposition
+  // to get prevstate, see line 66 to explain why we get prevposition
   function usePrevious(value) {
     // The ref object is a generic container whose current property is mutable ...
     // ... and can hold any value, similar to an instance property on a class
@@ -35,7 +39,7 @@ const MoveUser = ({ id }) => {
     // Return previous value (happens before update in useEffect above)
     return ref.current;
   }
-  //see line 58 to explain why we get prevposition
+  //see line 66 to explain why we get prevposition
   const prevPosition = usePrevious(position);
   useEffect(() => {
     // If a socket is defined  then emit to the server
@@ -44,25 +48,27 @@ const MoveUser = ({ id }) => {
         // i just want one user
         if (userexist) {
           socket.emit("new-user", {
+            store: storeId,
             id: id,
-            room: "Royal Palm",
-            MS: false,
+            role: role,
           });
           setUserexist(false);
         }
 
-        socket.emit("position", position, id, "Royal Palm");
+        socket.emit("position", position);
       });
     }
     /*
-    We want this useEffect to trigger when a position is changed
-    Also we dont want to recreate another socket to create a user
-    So this conditional statement will deliever that purpose
-    if a user exist  and a position is changed 
-    then do the following... emit a position...
+     
+     Since the useEffect already created a socket
+     we don't want to create another socket connection and
+     we use userexist to check that and to check if 
+    
+     and the reason we use prevPosition is because we want to check
+     if the position change  with prevPosition
     */
     if (userexist === false && prevPosition !== position) {
-      socket.emit("position", position, id, "Royal Palm");
+      socket.emit("position", position);
     }
 
     return () => {};
@@ -71,7 +77,7 @@ const MoveUser = ({ id }) => {
   const newuser = () => {
     //So it doesn't create a new socket
     if (connected) {
-      const socket = io(process.env.REACT_APP_endpoint);
+      const socket = io("http://localhost:3001");
       setSocket(socket);
       setConnected(false);
     }
@@ -80,28 +86,28 @@ const MoveUser = ({ id }) => {
     //  then set the position only when the button is clicked
     //  this conditionall statement is decieving but you can think of it that
     //  way (:
+
     if (userexist === false) {
       setPosition({
-        coords: {
-          accuracy: 65,
-          altitude: 210.50143432617188,
-          altitudeAccuracy: 10,
-          heading: -1,
-          latitude: position.coords.latitude + 0.0001,
-          longitude: -80.27,
-          speed: -1,
+        position: {
+          coords: {
+            accuracy: 65,
+            altitude: 210.50143432617188,
+            altitudeAccuracy: 10,
+            heading: -1,
+            latitude: position.position.coords.latitude + 0.0001,
+            longitude: -80.27,
+            speed: -1,
+          },
+          timestamp: 1610579477507.111,
         },
-        timestamp: 1610579477507.111,
+        storeId: storeId,
+        id: id,
       });
     }
   };
   return (
-    <Button
-      style={{ fontSize: 9, padding: 2, marginLeft: "3vh", marginRight: "3vh" }}
-      variant="outlined"
-      color="inherit"
-      onClick={() => newuser()}
-    >
+    <Button variant="outlined" color="inherit" onClick={() => newuser()}>
       Move User {id}
     </Button>
   );
